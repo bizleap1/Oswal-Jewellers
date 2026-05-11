b8a7a7e25a5e239820466d0a208cd739c407023b
Indrajit128 <indrajitkshirsagar128@gmail.com>
2026-05-11 13:12:26 +0530

    Remove Heritage Artifacts and About Us card from SilverArtifacts

--- src/components/SilverArtifacts.jsx
+++ src/components/SilverArtifacts.jsx
@@ -21,12 +21,7 @@ const SilverArtifacts = () => {
       img: "/images/silver_diya_collection.jpg",
       link: "/collections"
     },
-    {
-      id: 4,
-      title: "Heritage Artifacts",
-      img: "/images/silver_heritage_new.jpg",
-      link: "/collections"
-    }
+
   ];
 
   return (
@@ -56,20 +51,7 @@ const SilverArtifacts = () => {
             </div>
           ))}
 
-          {/* About Us Integration Card */}
-          <div className="reel-card about-reel">
-            <Link to="/about">
-              <div className="reel-img">
-                <img src="/images/silver_legacy_new.jpg" alt="About Oswal Jewellers" />
-                <div className="reel-overlay about-overlay">
-                  <span className="about-label">SINCE 20+ YEARS</span>
-                  <h3>Our Legacy</h3>
-                  <p>Discover the journey of Oswal Jewellers</p>
-                  <span className="btn-small">Learn More →</span>
-                </div>
-              </div>
-            </Link>
-          </div>
+
         </div>
       </div>
     </section>
