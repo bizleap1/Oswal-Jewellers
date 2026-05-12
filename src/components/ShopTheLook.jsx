c9de016ec407346eba6577152b326ef472a6482c
Indrajit128 <indrajitkshirsagar128@gmail.com>
2026-05-12 11:59:20 +0530

    Remove Gold Studs and Diamond Choker hotspots from ShopTheLook

--- src/components/ShopTheLook.jsx
+++ src/components/ShopTheLook.jsx
@@ -7,21 +7,6 @@ const ShopTheLook = () => {
         <div className="look-grid">
           <div className="look-main-img animate-fade-up">
             <img src="/images/Bridal Gold Jewelry Full Set.jpg" alt="Styled Model" />
-            
-            {/* Hotspots */}
-            <div className="hotspot" style={{ top: '30%', left: '50%' }}>
-              <div className="hotspot-dot"></div>
-              <div className="hotspot-card">
-                <span>Diamond Choker</span>
-              </div>
-            </div>
-            
-            <div className="hotspot" style={{ top: '45%', left: '42%' }}>
-              <div className="hotspot-dot"></div>
-              <div className="hotspot-card">
-                <span>Gold Studs</span>
-              </div>
-            </div>
           </div>
           
           <div className="look-info animate-fade-up" style={{ animationDelay: '0.2s' }}>
